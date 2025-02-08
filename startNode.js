077d1c8d64bf72df3ccd969f9705bb21b6ba9935
Teemo0909 <106057493+Teemo0909@users.noreply.github.com>
2025-02-08 19:54:43 +0700

    TEST1

---
+++ startNode.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const studentId = '66080904';
+const studentName = 'Kannisa Intra';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`CSI283: DIGITAL ARCHITECTURE AND OPERATING SYSTEM\nThis is a sample learning tool for the CSI 283 course.\nWorkshop #3: startNode.js\nStudent ID: ${studentId}\nStudent Name: ${studentName}\nHELLO`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
